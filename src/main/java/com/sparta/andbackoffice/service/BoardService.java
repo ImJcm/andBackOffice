0e4dda64fe31d94ecb42fce541adf50bc6c0788f
ImJcm <wjsckdals0909@naver.com>
2024-06-21 17:18:05 +0900

    FIX : 메서드 설명 수정

--- src/main/java/com/sparta/andbackoffice/service/BoardService.java
+++ src/main/java/com/sparta/andbackoffice/service/BoardService.java
@@ -5,40 +5,44 @@ import com.sparta.andbackoffice.dto.response.ApiResponseDto;
 import com.sparta.andbackoffice.dto.response.BoardResponseDto;
 import com.sparta.andbackoffice.entity.Board;
 
+import java.util.List;
+
 public interface BoardService {
 
 	/**
 	 * 게시글 생성
-	 * @param categoryId : 카테고리
 	 * @param requestDto : 작성할 내용
 	 * @return : 생성한 글 내용 반환
 	 */
-	BoardResponseDto createBoard(Long categoryId, BoardRequestDto requestDto);
+	BoardResponseDto createBoard(BoardRequestDto requestDto);
 
 	/**
 	 * 게시글 단건 조회
-	 * @param categoryId : 카테고리
 	 * @param boardId    : 조회할 글 id
 	 * @return : 게시글 정보 반환
 	 */
-	BoardResponseDto getBoard(Long categoryId, Long boardId);
+	BoardResponseDto getBoard( Long boardId);
+
+	/**
+	 * 게시글 전체 조회
+	 * @return : 게시글 리스트 반환
+	 */
+	List<BoardResponseDto> getAllBoards();
 
 	/**
 	 * 게시글 수정
-	 * @param categoryId : 카테고리
 	 * @param boardId    : 수정할 게시글 id
 	 * @param requestDto : 수정할 내용
 	 * @return : 수정된 게시글 내용 반환
 	 */
-	BoardResponseDto modifyBoard(Long categoryId, Long boardId, BoardRequestDto requestDto);
+	BoardResponseDto modifyBoard(Long boardId, BoardRequestDto requestDto);
 
 	/**
 	 * 게시글 삭제
-	 * @param categoryId : 카테고리
 	 * @param boardId    : 삭제할 게시글 id
 	 * @return : 요청 처리 메시지 + 상태코드
 	 */
-	ApiResponseDto deleteBoard(Long categoryId, Long boardId);
+	ApiResponseDto deleteBoard(Long boardId);
 
 	/**
 	 * 존재하는 카테고리인지 검증
@@ -59,4 +63,6 @@ public interface BoardService {
 	 * @param board      : 게시글 정보 - 실제 글이 위치한 카테고리 id
 	 */
 	void equalsCategory(Long categoryId, Board board);
+
+
 }
