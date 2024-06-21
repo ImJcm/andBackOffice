2e43bdfff4c8b70cf83ade1b47b154bc9b4ad50e
ImJcm <wjsckdals0909@naver.com>
2024-06-21 16:31:19 +0900

    FIX : 불필요 개행 문자 제거

--- src/main/java/com/sparta/andbackoffice/service/CommentService.java
+++ src/main/java/com/sparta/andbackoffice/service/CommentService.java
@@ -10,7 +10,6 @@ public interface CommentService {
 
 	/**
 	 * 댓글 전체 조회 (댓글 원본 표시)
-	 *
 	 * @param postId 게시글 id
 	 * @return 게시글의 전체 댓글 리스트
 	 */
@@ -18,7 +17,6 @@ public interface CommentService {
 
 	/**
 	 * 댓글 삭제
-	 *
 	 * @param commentId 삭제할 댓글 ID
 	 * @return 요청 결과
 	 */
@@ -26,7 +24,6 @@ public interface CommentService {
 
 	/**
 	 * 댓글 조회
-	 *
 	 * @param commentId 조회할 댓글 ID
 	 * @return Comment or IllegalException
 	 */
